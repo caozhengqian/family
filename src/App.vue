200a88c08f095ddd4c7c3b2113d92e466da19aaf
1264419544@qq.com <1264419544@qq.com>
2021-02-02 00:02:00 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <video style="width:300px;height:450px" controls="" autoplay="" name="media"><source src="./assets/video/02930943ef6e7ed80fe6a4d7fafc42ae.mp4" type="video/mp4"></video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
